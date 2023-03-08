c5eaaaffaeccf7328b6ec1efd70be30d6b495359
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-03-08 17:40:40 -0600

    Feedback

--- exercise5.js
+++ exercise5.js
@@ -1,13 +1,7 @@
 function vowelsDigits(inpt) {
   if (typeof inpt === "number") {
     // Logaritmic solution:
-    return (
-      "-" +
-      inpt +
-      "- has: " +
-      String(Math.floor(Math.log10(inpt)) + 1) +
-      " digits"
-    );
+    return Math.floor(Math.log10(inpt)) + 1;
   }
   // Vowels solution
   // I used a regular expression to extract the
@@ -16,11 +10,9 @@ function vowelsDigits(inpt) {
     const regexVowels = /[aeiouAEIOU]/g;
     let matches = inpt.match(regexVowels);
     if (!matches) {
-      return "-" + inpt + "- has -" + 0 + "- vowels";
+      return 0;
     } else {
-      return (
-        "-" + inpt + "- has -" + inpt.match(regexVowels).length + "- vowels"
-      );
+      return inpt.match(regexVowels).length;
     }
   }
 }
