7cecc87186babb89dceed7fc253b90f259b4314c
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-03-08 17:29:07 -0600

    Feedback

--- exercise2.js
+++ exercise2.js
@@ -1,23 +1,28 @@
-// Solution based on the use of a closure to limit
-// the number of executions.
-function limitFunc(fn, num) {
-  let counter = 0;
-  return () => {
-    counter++;
-    if (counter <= num) {
-      fn();
+// Define limitFunc function with fn and limit arguments
+function limitFunc(fn, limit) {
+  // Initialize counter variable
+  let count = 0;
+  // Define limited function that accepts any number of arguments
+  function limited(...args) {
+    // Throw error if counter variable exceeds limit
+    if (count >= limit) {
+      throw new Error(`Function can only be called ${limit} times`);
     }
-  };
+    // Increment counter and call fn with arguments
+    count++;
+    return fn(...args);
+  }
+  // Return limited function
+  return limited;
 }
 
-function exe() {
-  console.log("Executed");
-}
-let limited = limitFunc(exe, 2);
-limited();
-limited();
-limited();
-limited();
-limited();
-limited();
-limited();
+// Example usage
+// Define add function that returns sum of two arguments
+const add = (a, b) => a + b;
+// Create limitedAdd function with limit of 2
+const limitedAdd = limitFunc(add, 2);
+// Call limitedAdd function with arguments
+console.log(limitedAdd(1, 2)); // Output: 3
+console.log(limitedAdd(3, 4)); // Output: 7
+// Call limitedAdd function with more arguments, should throw error
+console.log(limitedAdd(5, 6)); // Output: Error: Function can only be called 2 times
